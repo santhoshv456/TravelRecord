d547d0c61396841caba29718dcd5d0b9da4acc01
santhosh <santhoshv456@gmail.com>
2020-10-14 00:02:17 +0530

    first commit

---
+++ TravelRecord/TravelRecord/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecord
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUserName.Text) || string.IsNullOrEmpty(txtPassword.Text))
+            {
+                return;
+            }
+
+            Navigation.PushAsync(new HomePage());
+        }
+    }
+}
